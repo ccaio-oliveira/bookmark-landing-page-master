c214b8403f8536a4ef41c6b60da5fe80fe460381
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-17 23:29:51 -0300

    finishing soeedy searching

--- src/components/Section02/SpeedySearching/Speed.jsx
+++ src/components/Section02/SpeedySearching/Speed.jsx
@@ -1,6 +1,20 @@
 const Speed = () => {
-    return(
-        <h1>Speedy Searching</h1>
+    return (
+        <div className="content">
+            <div className="img">
+                <img src="./img/illustration-features-tab-2.svg" alt="Illustration Hero" />
+                <div className="formBackImg"></div>
+            </div>
+            <div className="text">
+                <h1>Intelligent search</h1>
+                <p className="subTitle">
+                    Our powerful search feature will help you find saved <br /> sites 
+                    in no time at all. No need to trawl through all of <br /> your 
+                    bookmarks.
+                </p>
+                <button className="btnGeral">More Info</button>
+            </div>
+        </div>
     )
 }
 

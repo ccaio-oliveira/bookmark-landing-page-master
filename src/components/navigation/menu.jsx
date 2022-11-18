9bd47630b36d72ce309d0cceda04e6e84f4c7b56
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-18 00:44:08 -0300

    finalizando menu

--- src/components/navigation/menu.jsx
+++ src/components/navigation/menu.jsx
@@ -1,26 +1,51 @@
+import { useState } from 'react';
 import './menu.css';
 
 const Menu = () => {
+    const [menu, setMenu] = useState('./img/icon-hamburger.svg');
+    const [closeOpen, setCloseOpen] = useState('close');
+
+    const changeMenu = () => {
+        if (menu === './img/icon-hamburger.svg') {
+            setMenu('./img/icon-close.svg');
+            setCloseOpen('open');
+        } else {
+            setMenu('./img/icon-hamburger.svg');
+            setCloseOpen('close');
+        }
+    }
     return (
         <div className="navigation">
             <div className="container">
-                <a href="#"><img src="./img/logo-bookmark.svg" alt="Logo" id="logo" /></a>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#features">FEATURES</a>
-                        </li>
-                        <li>
-                            <a href="#pricing">PRICING</a>
-                        </li>
-                        <li>
-                            <a href="#contact">CONTACT</a>
-                        </li>
-                        <li>
-                            <a href="#" id="btnLogin">LOGIN</a>
-                        </li>
-                    </ul>
-                </nav>
+                <a href="/"><img src="./img/logo-bookmark.svg" alt="Logo" id="logo" /></a>
+                <div className="menu" onClick={changeMenu}>
+                    <img src={menu} alt="Menu Icon" id='menuIcon' />
+                </div>
+
+                <div id="menuToggle" className={`${closeOpen}`}>
+                    <div className="up">
+                        <a href="/"><img src="./img/logo-bookmark.svg" alt="Logo" id="logo" /></a>
+                        <div className="menu" onClick={changeMenu}>
+                            <img src={menu} alt="Menu Icon" id='menuIcon' />
+                        </div>
+                    </div>
+                    <div id="navigation">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#features">FEATURES</a>
+                                </li>
+                                <li>
+                                    <a href="#pricing">PRICING</a>
+                                </li>
+                                <li>
+                                    <a href="#contact">CONTACT</a>
+                                </li>
+                                <button id="btnLogin">LOGIN</button>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
             </div>
         </div>
     )

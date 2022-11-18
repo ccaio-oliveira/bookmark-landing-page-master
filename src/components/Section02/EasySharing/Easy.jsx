3b61f0f2cbb6639f593fbe89cf44170cd1416e55
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-17 23:32:42 -0300

    finishing features

--- src/components/Section02/EasySharing/Easy.jsx
+++ src/components/Section02/EasySharing/Easy.jsx
@@ -1,6 +1,20 @@
 const Easy = () => {
     return(
-        <h1>Easy Sharing</h1>
+        <div className="content">
+            <div className="img">
+                <img src="./img/illustration-features-tab-3.svg" alt="Illustration Hero" />
+                <div className="formBackImg"></div>
+            </div>
+            <div className="text">
+                <h1>Share your bookmarks</h1>
+                <p className="subTitle">
+                    Easily share your bookmarks and collections with <br /> others.
+                    Create a shareable link that you can send at <br /> the click
+                    of a button.
+                </p>
+                <button className="btnGeral">More Info</button>
+            </div>
+        </div>
     )
 }
 
